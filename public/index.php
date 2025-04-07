7bfdbdd1725e679413e0447adb1f5ae84aed4c67
KevinJ Saldaña <kjoseph1910@hotmail.com>
2025-04-07 18:46:05 +0200

    crud vendedor finalizado

--- public/index.php
+++ public/index.php
@@ -4,17 +4,26 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controller\PropiedadController;
+use Controller\VendedorController;
 
 $router = new Router();
 
 $router->get('/admin', [PropiedadController::class, 'index']);
-$router->post('/propiedad/delete', [PropiedadController::class, 'delete']);
+$router->post('/propiedades/eliminar', [PropiedadController::class, 'delete']);
 
-$router->get('/propiedad/crear', [PropiedadController::class, 'create']);
-$router->post('/propiedad/crear', [PropiedadController::class, 'create']);
+$router->get('/propiedades/crear', [PropiedadController::class, 'create']);
+$router->post('/propiedades/crear', [PropiedadController::class, 'create']);
 
-$router->get('/propiedad/actualizar', [PropiedadController::class, 'update']);
-$router->post('/propiedad/actualizar', [PropiedadController::class, 'update']);
+$router->get('/propiedades/actualizar', [PropiedadController::class, 'update']);
+$router->post('/propiedades/actualizar', [PropiedadController::class, 'update']);
 
+
+$router->get('/vendedores/crear', [VendedorController::class, 'create']);
+$router->post('/vendedores/crear', [VendedorController::class, 'create']);
+
+$router->get('/vendedores/actualizar', [VendedorController::class, 'update']);
+$router->post('/vendedores/actualizar', [VendedorController::class, 'update']);
+
+$router->post('/vendedores/eliminar', [VendedorController::class, 'delete']);
 
 $router->checkRoutes();
